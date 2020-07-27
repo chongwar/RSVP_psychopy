acca4a221b6e9d2b1d2223511be35e687d474e1b
chongwar <chongwar@163.com>
2020-07-27 17:41:59 +0800

    initial the repository

---
+++ biosemi.py
@@ -0,0 +1,81 @@
+import socket
+import numpy as np
+
+
+class ActiveTwo():
+    def __init__(self, host='127.0.0.1', sfreq=1024, 
+                 port=1111, nchannels=65, tcpsamples=4):
+        """
+        Initialize connection and parameters of the signal
+        :param host: IP address where ActiView is running
+        :param port: Port ActiView is listening on
+        :param nchannels: Number of EEG channels
+        """
+        
+        self.host = host
+        self.port = port
+        self.nchannels = nchannels
+        self.sfreq = sfreq
+        self.tcpsamples = tcpsamples
+        self.buffer_size = self.nchannels * self.tcpsamples * 3
+
+        # open connection
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((self.host, self.port))
+
+    def read(self, duration):
+        """
+        Read signal from the EEG device
+        :param duration: How long to read in seconds
+        :return: Signal in the matrix form: (samples, channels)
+        """
+
+        # initialize final data array
+        rawdata = np.empty((0, self.nchannels))
+
+        # The reader process will run until requested amount of data is collected
+        samples = 0
+        while samples < duration * self.sfreq:
+
+            # Create a 16-sample signal_buffer
+            signal_buffer = np.zeros((self.nchannels, self.tcpsamples))
+
+            # Read the next packet from the network
+            # sometimes there is an error and packet is smaller than needed
+            # read until get a good one
+            data = []
+            while len(data) != self.buffer_size:
+                data = self.s.recv(self.buffer_size)
+
+
+            # Extract 16 samples from the packet (ActiView sends them in 16-sample chunks)
+            for m in range(self.tcpsamples):
+                # extract samples for each channel
+                for ch in range(self.nchannels):
+                    offset = m * 3 * self.nchannels + (ch * 3)
+
+                    # The 3 bytes of each sample arrive in reverse order
+                    # sample = (ord(data[offset+2]) << 16)
+                    # sample += (ord(data[offset+1]) << 8)
+                    # sample += ord(data[offset])
+                    sample = (data[offset+2] << 16)
+                    sample += (data[offset+1] << 8)
+                    sample += data[offset]
+
+                    # Store sample to signal buffer
+                    signal_buffer[ch, m] = sample
+
+            # update sample counter
+            samples += self.tcpsamples
+
+            # transpose matrix so that rows are samples
+            signal_buffer = np.transpose(signal_buffer)
+
+            # add to the final dataset
+            rawdata = np.concatenate((rawdata, signal_buffer), axis=0)
+            
+            self.s.close()
+        return rawdata
+    
+        
+    
---
+++ rsvp_main.py
@@ -0,0 +1,88 @@
+import os
+import warnings
+import numpy as np
+from win32api import GetSystemMetrics
+from psychopy import visual, core, parallel
+from biosemi import ActiveTwo
+
+warnings.filterwarnings('ignore')
+
+
+def generate_texture(win, img_dir):
+    img_num = len(os.listdir(img_dir))
+    texture = []
+    for _img in os.listdir(img_dir):
+        img = os.path.join(img_dir, _img)
+        texture_tmp = visual.ImageStim(win, image=img)
+        texture.append(texture_tmp)
+    return img_num, texture
+
+
+def draw_fixation(win, interval=2):
+    length, width = 120, 3.5
+    _w, _h = length / w, length / h
+    l, r, u, d = (-_w, 0), (_w, 0), (0, -_h), (0, _h)
+    line_lr = visual.Line(win, l, r, lineWidth=width)
+    line_ud = visual.Line(win, u, d, lineWidth=width)
+    line_lr.draw()
+    line_ud.draw()
+    win.flip()
+    core.wait(interval)
+
+
+# initial parallel port
+p_port = parallel.ParallelPort(address=0xAFF8)
+
+w = GetSystemMetrics(0)
+h = GetSystemMetrics(1)
+win = visual.Window(size=(w, h), fullscr=False, color=(-1, -1, -1))
+
+# generate target and notarget textures
+tar_dir = 'images/tar'
+notar_dir = 'images/notar'
+tar_num, texture_tar = generate_texture(win, tar_dir)
+notar_num, texture_notar = generate_texture(win, notar_dir)
+
+# parameters
+host = '10.170.33.219'
+sfreq = 1024
+port = 1111
+channle_num = 65
+trial_num = 2
+seq_num = 50
+display_time = 0.05
+duration = seq_num * display_time + 0.5
+tar_posi = [5, 15, 25, 35]
+data = []
+
+for trial in range(trial_num):
+    draw_fixation(win)
+    tar_rand_idx = np.random.permutation(tar_num)
+    notar_rand_idx = np.random.permutation(notar_num)
+    tar_count = 0
+    notar_count = 0
+    active_two = ActiveTwo(host=host, sfreq=sfreq, 
+                           port=port, nchannels=channle_num)
+    for seq in range(seq_num):
+        if seq in tar_posi:
+            trigger = 4
+            texture_tar[tar_rand_idx[tar_count]].draw()
+            tar_count += 1
+        else:
+            trigger = 16 if seq == 0 else 1
+            texture_notar[notar_rand_idx[notar_count]].draw()
+            notar_count += 1
+        
+        p_port.setData(trigger)  
+        win.flip()
+        core.wait(display_time)
+    
+    win.flip()
+    raw_data = active_two.read(duration=duration)
+    data.append(raw_data)
+    core.wait(1)
+
+win.close()
+
+np.save('data/block_1', np.array(data))
+print('Finish.')
